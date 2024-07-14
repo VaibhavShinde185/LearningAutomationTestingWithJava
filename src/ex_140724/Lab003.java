975b7de6e544985ca616e3a086abc989b3a41b9a
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-07-14 18:27:25 +0530

    New file added

---
+++ src/ex_140724/Lab003.java
@@ -0,0 +1,19 @@
+package ex_140724;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //  Data Types
+        //Primitive Data Type
+        //Boolean
+            //true or false
+        //Numerical - byte, int, short, long, char
+        //Floating - float, double
+        // eg. we cant store a 128 in byte
+        byte a = 128;
+        //Non Primitive Data Type - string, array, class, objects, enum
+
+        //Constants
+
+
+    }
+}
