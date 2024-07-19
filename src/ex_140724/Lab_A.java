745235c0319c37757b381e8873ba1f4e5a9fe4e4
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-07-19 10:12:55 +0530

    New file added

---
+++ src/ex_140724/Lab_A.java
@@ -0,0 +1,21 @@
+package ex_140724;
+
+public class Lab_A {
+    public static void main(String[] args) {
+        //
+       // int a = "pramod";
+
+        boolean b = true;
+        boolean b1 = false;
+//        boolean b12 = "yes";
+//        boolean b2 = 'A'; Not possible
+
+        float f = 129.8763e2F;
+
+        String str2=null;
+        System.out.println(str2);
+
+
+
+    }
+}
--- src/ex_140724/Operators.java
+++ src/ex_140724/Operators.java
@@ -2,9 +2,49 @@ package ex_140724;
 
 public class Operators {
     public static void main(String[] args) {
-        double a=34;
-        double b=10;
-        double result=a%b;
+        double c=34;
+        double d=10;
+        double result=c%d;
         System.out.println(result);
+
+        // ✅ Operators
+        int age = 65; // 10000001 -> switch off/ on
+        // = Assignment Operator - Assign the value literal to the ref of data type
+
+        // Arithmetic Operator +,-,/,*,
+        // Modulus Operator %
+        int a = 10;
+        int b = 23;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b); // Remainders
+
+        // Unary Operator
+        age = +65;  //unary plus
+        int lambo = -1; //unary minus
+        System.out.println(age);
+        System.out.println(lambo+2);
+
+        //Concatination
+        String firstname = "VAIBHAV";
+        String lastname = "SHINDE";
+        System.out.println(firstname + " " + lastname);
+
+        // Compound Assignment Operators: += , -=, /=, *= . %=, += ->
+        a = 10;
+
+        a+=10; // a = a+10
+        a-=10; // a = a-10
+        a/=10; // a = a/10
+        a*=10; // a = a*10
+
+        // Relational Operators -> boolean (true or false), >, < >=,<=, == , != ( ! = )
+        a = 10;
+        b = 20;
+        int sum = a+b; // operand a, b , operator =, +
+
+
     }
 }
