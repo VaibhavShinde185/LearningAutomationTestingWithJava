6b259746dd148c55eff285f2829be2c094e46ae2
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-08-01 23:07:42 +0530

    New file added

---
+++ src/practiceArray/maxMinElement.java
@@ -0,0 +1,40 @@
+package practiceArray;
+
+import java.util.Scanner;
+
+public class maxMinElement {
+    public static int min(int[] a){
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if(min < a[i]){
+                min = a[i];
+            }
+        }
+        return min;
+    }
+    public static int max(int[] a){
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if(max > a[i]){
+                max = a[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Enter a integer " + (i+1));
+            a[i] = sc.nextInt();
+        }
+
+        int min = min(a);
+        int max = max(a);
+
+        System.out.println("Minimum number is " + min);
+        System.out.println("Maximum number is " + max);
+    }
+}
+
--- src/practice_Lab2.java
+++ src/practice_Lab2.java
@@ -2,13 +2,31 @@
 // Find the Max Salary, Min  in the Salary Array
 
 
+import java.util.Scanner;
+
 public class practice_Lab2 {
+    public static int addition(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+        }
+        return sum;
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
 
-
-            }
-
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Enter a integer " + (i+1));
+            a[i] = sc.nextInt();
+        }
+        int sum = addition(a);
+        System.out.println("Addition of array element is " + sum);
+    }
 }
+
+
+
 
 
 // Lab147  -- sort function
