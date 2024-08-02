cbe6c509b98bcedc166939647f661037ede49a58
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-08-02 23:03:44 +0530

    New file added

---
+++ src/practiceArray/array2DInput.java
@@ -0,0 +1,28 @@
+package practiceArray;
+
+import java.util.Scanner;
+
+public class array2DInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[3][5];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print("Enter value for a[" + i +"]" + "[" + j + "]: ");
+                a[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        System.out.println("Entered array elements are ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j]);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
--- src/practice_Lab2.java
+++ src/practice_Lab2.java
@@ -5,27 +5,31 @@
 import java.util.Scanner;
 
 public class practice_Lab2 {
-    public static int addition(int[] a) {
-        int sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum = sum + a[i];
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            int[][] a = new int[3][5];
+
+            // Input array elements
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[i].length; j++) {
+                    System.out.print("Enter value for a[" + i + "][" + j + "]: ");
+                    a[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.println("Entered array elements are:");
+
+            // Print array elements with indices
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[i].length; j++) {
+                    // Highlighted part: printing index with the value
+                    System.out.print("a[" + i + "][" + j + "]=" + a[i][j] + "\t");
+                }
+                System.out.println();
+            }
         }
-        return sum;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] a = new int[5];
-
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("Enter a integer " + (i+1));
-            a[i] = sc.nextInt();
-        }
-        int sum = addition(a);
-        System.out.println("Addition of array element is " + sum);
-    }
-}
-
-
 
 
 
