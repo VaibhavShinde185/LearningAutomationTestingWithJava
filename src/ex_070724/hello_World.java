711c52d54a091f9235e39bc3aadb3d6d4257330d
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-10-08 15:55:34 +0530

    New file added

--- src/ex_070724/hello_World.java
+++ src/ex_070724/hello_World.java
@@ -2,6 +2,8 @@ package ex_070724;
 
 public class hello_World {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Learning Java for API and Web automation");
     }
 }
