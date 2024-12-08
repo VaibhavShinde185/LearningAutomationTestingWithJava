497af37433feb8cd22645bfc91846a50b9af7aae
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-12-08 19:53:26 +0530

    New file added

--- src/practice_Lab.java
+++ src/practice_Lab.java
@@ -5,21 +5,34 @@ import java.util.Scanner;
 public class practice_Lab {
     public static void main(String[] args) {
 
+        String str1 = "Hello";
+        String str2 = "";
+        char ch;
+
+        for (int i = 0; i < str1.length(); i++) {
+            ch = str1.charAt(i);
+            str2 = ch+str2;
+            }
+        System.out.println("Reversed String: "+ str2);
+        System.out.println("---------------------------------------------------------------");
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String: ");
+        System.out.println("Enter a string: ");
         String original = sc.nextLine();
 
         String sanitized = original.replaceAll("\\s+","").toLowerCase();
 
         String reversed = "";
-        for (int i = sanitized.length()-1; i >= 0 ; i--) {
+        for (int i = sanitized.length() - 1; i >= 0 ; i--) {
             reversed += sanitized.charAt(i);
         }
         if (sanitized.equals(reversed)){
-            System.out.println("\" " + original + "\" is a palindrome. ");
+            System.out.println("\""+original+"\" it is pallindrome");
         } else {
-            System.out.println("\" " + original + "\" is not a palindrome. ");
+            System.out.println("\""+original+"\" it is not pallindrome");
+
         }
+
         }
     }
 
--- src/practice_Lab2.java
+++ src/practice_Lab2.java
@@ -4,21 +4,37 @@ import java.util.Scanner;
 public class practice_Lab2 {
         public static void main(String[] args) {
 
-            int[][] array_2d = {
-                    {34, 1},
-                    {34, 2},
-                    {34, 3}
-            };
+        String str1 = "Vaibhav";
+        String str2 = "";
+        char ch;
 
-            for (int i = 0; i < array_2d.length; i++) {
-                for (int j = 0; j < array_2d[i].length; j++) {
-                    System.out.print(array_2d[i][j]);
-                    System.out.print("\t");
-                }
-                System.out.println();
+            System.out.println("Original String: " + str1);
+
+            for (int i = 0; i < str1.length(); i++) {
+                ch = str1.charAt(i);
+                str2 = ch+str2;
             }
+            System.out.println("Reversed String: "+ str2);
+            System.out.println("-------------------------------------------");
 
+            System.out.print("\n");
 
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            String original = sc.nextLine();
+
+            String sanitized = original.replaceAll("\\s+","").toLowerCase();
+            System.out.println("Sanitized string: "+sanitized);
+
+            String reversed = "";
+            for (int i = sanitized.length() - 1; i >= 0 ; i--) {
+                reversed += sanitized.charAt(i);
+            }
+            if(sanitized.equals(reversed)){
+                System.out.println("\""+ original + "\" it is a palindrome");
+            } else {
+                System.out.println("\""+ original + "\" it is not a palindrome");
+            }
         }
     }
 
