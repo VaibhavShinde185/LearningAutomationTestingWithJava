76df540ed210fe600fc14b9c4b7a87f9adfdbbc0
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2025-03-06 14:16:31 +0530

    New file added

--- src/ex_270724_arrays/arrays_3.java
+++ src/ex_270724_arrays/arrays_3.java
@@ -6,6 +6,7 @@ public class arrays_3 {
         String weekdays[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
         System.out.println("Length of the array is: " + weekdays.length);
         System.out.println(weekdays[6]);
+        System.out.println("--------------------------------------------------");
 
         for(int i=0;i< weekdays.length; i++){
             System.out.println(weekdays[i]);
--- src/practiceJava/practice_Lab3.java
+++ src/practiceJava/practice_Lab3.java
@@ -1,23 +1,23 @@
 package practiceJava;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class practice_Lab3 {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100 ; i++) {
+     int[][] array2d = {
+             {34,1},
+             {34,2},
+             {34,3}
+     };
 
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("Fizz - Buzz");
-            } else if (i % 3 == 0){
-                System.out.println("Fizz");
-            } else if (i % 5 == 0){
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+        for (int i = 0; i < array2d.length; i++) {
+            for (int j = 0; j < array2d[i].length; j++) {
+                System.out.print(array2d[i][j]);
+                System.out.print("\t");
             }
-
+            System.out.println();
         }
-
     }
 }
