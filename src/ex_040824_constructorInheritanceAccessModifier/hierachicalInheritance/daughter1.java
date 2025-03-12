922ff88e523fd6f2e211987b51ca61741ae7cbaf
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2025-03-12 12:01:04 +0530

    New file added

--- src/ex_040824_constructorInheritanceAccessModifier/hierachicalInheritance/daughter1.java
+++ src/ex_040824_constructorInheritanceAccessModifier/hierachicalInheritance/daughter1.java
@@ -1,7 +1,7 @@
 package ex_040824_constructorInheritanceAccessModifier.hierachicalInheritance;
 
 public class daughter1 extends father{
-    void home3(){
+    void home4(){
         System.out.println("Home 4 - child three");
 
     }
