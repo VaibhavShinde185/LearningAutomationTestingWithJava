882081a27d12fbd635a3a11608a1a6a9f1ad3e81
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2025-03-12 08:19:06 +0530

    New file added

--- src/ex_040824_constructorInheritanceAccessModifier/accessModifiers/accessModifiers.java
+++ src/ex_040824_constructorInheritanceAccessModifier/accessModifiers/accessModifiers.java
@@ -5,7 +5,7 @@ public class accessModifiers {
 //    private
 //    protected
 //    default
-// 2 publix class are not allowed
+// 2 public class are not allowed
 //  If you do not type anything before declaring class it is default access modifier
 
 }
