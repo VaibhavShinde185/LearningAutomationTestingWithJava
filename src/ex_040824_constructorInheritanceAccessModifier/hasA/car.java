ab163069ef9dca72c76fa037b3afd3018d12b1bd
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2025-06-23 09:38:41 +0530

    New file added

--- src/ex_040824_constructorInheritanceAccessModifier/hasA/car.java
+++ src/ex_040824_constructorInheritanceAccessModifier/hasA/car.java
@@ -1,15 +1,24 @@
 package ex_040824_constructorInheritanceAccessModifier.hasA;
 
+import ex_040824_constructorInheritanceAccessModifier.hasA.isA.tata;
+
 public class car {
     // Aggregation - hasA relationship
     // hasA relation does  not use any inheritance concept
     protected void startTheCar(){
         new engine().start();
         new tyres().rolling();
+
     }
 
     public static void main(String[] args) {
         car c = new car();
         c.startTheCar();
+
+        tata t = new tata();
+        t.start();
     }
+
+
+
 }
--- src/ex_040824_constructorInheritanceAccessModifier/hasA/isA/tata.java
+++ src/ex_040824_constructorInheritanceAccessModifier/hasA/isA/tata.java
@@ -2,7 +2,7 @@ package ex_040824_constructorInheritanceAccessModifier.hasA.isA;
 import ex_040824_constructorInheritanceAccessModifier.hasA.car;
 
 public class tata extends car {
-    void start(){
+    public void start(){
         startTheCar();
     }
 
