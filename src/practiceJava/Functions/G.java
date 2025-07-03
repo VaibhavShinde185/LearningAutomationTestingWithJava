a5308a83d48f3f71b03759492da6441abacf7ba7
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2025-07-03 17:15:38 +0530

    New file added

--- src/practiceJava/Functions/G.java
+++ src/practiceJava/Functions/G.java
@@ -7,6 +7,8 @@ public class G {
     public static void main(String[] args) {
         int num1 = 12, num2 = 18;
         System.out.println("The GCD of " + num1 + " and " + num2 + " is " + findGCD(num1, num2));
+        System.out.println("The lcm of " + num1 + " and " + num2 + " is " + findLCM(num1, num2));
+
     }
 
     // Function that accepts two numbers and returns their GCD
@@ -18,6 +20,12 @@ public class G {
             a = temp;   // Assign b to a
         }
         return a;  // When b becomes 0, a is the GCD
+    }
+
+
+    static int findLCM(int a, int b){
+        int lcm = (a * b) / findGCD(a, b);
+        return lcm;
     }
 
 }
--- src/practice_Lab2.java
+++ src/practice_Lab2.java
@@ -1,32 +1,34 @@
 import ex_180824.ThrowsAndThrow.Bank;
 
-import javax.sound.midi.Soundbank;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.lang.invoke.SwitchPoint;
-import java.util.*;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.Scanner;
 
 public class practice_Lab2 {
     public static void main(String[] args) {
-
-
-        Stack s = new Stack();
-        s.push("Vaibhav");
-        s.push("Viraj");
-        s.push("VVVVVV");
-        s.push("BMC");
-        s.push("AMC");
-        s.push("MOI");
-
-        System.out.println(s);
-        System.out.println(s.peek());
-        System.out.println(s.pop());
-        System.out.println(s);
-        System.out.println(s.push("AMC"));
-        System.out.println(s);
-
-
+        int num1 = 12, num2 = 18;
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + findGCD(num1, num2));
+        System.out.println("The lcm of " + num1 + " and " + num2 + " is " + findLCM(num1, num2));
 
     }
+
+        static int findGCD(int a, int b){
+
+            while (b != 0){
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+        return  a;
+        }
+
+        static int findLCM(int a, int b){
+
+            int lcm = (a * b) / findGCD(a, b);
+            return lcm;
+        }
+
 }
+
+
